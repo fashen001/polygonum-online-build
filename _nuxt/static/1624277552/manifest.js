104d05f0601f6695d022373efeb00a494935882e
Nikita Marcius <nikita.marcius@gmail.com>
2021-07-01 22:24:30 +0300

    +

--- _nuxt/static/1624277552/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fteam","\u002Ffaq","\u002Froadmap","\u002Ftoken"]})
---
+++ _nuxt/static/1625167415/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fteam","\u002Ftoken","\u002Ffaq","\u002Froadmap"]})
